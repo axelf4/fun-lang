956cf380bb220eeb64664792e1c1553d40ab1da4
Axel Forsman <axelsfor@gmail.com>
2022-01-13 17:27:57 +0100

    Add let bindings

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,9 +1,15 @@
+/// Lexical analysis.
 use logos::Logos;
 use std::fmt;
 use std::ops::Range;
 
 #[derive(Logos, Clone, Copy, PartialEq, Debug)]
 pub enum Token<'input> {
+    #[regex("-?[0-9]+", |lex| lex.slice().parse(), priority = 100)]
+    Number(i32),
+    #[regex(r#"[[:^space:]--[\\.()@"]]+"#)]
+    Ident(&'input str),
+
     #[token(r"\")]
     BackSlash,
     #[token("->")]
@@ -12,21 +18,23 @@ pub enum Token<'input> {
     LParen,
     #[token(")")]
     RParen,
+    #[token("=")]
+    Equals,
+    #[token(":")]
+    Colon,
 
     #[token("case")]
     Case,
     #[token("of")]
     Of,
+    #[token("let")]
+    Let,
+    #[token("in")]
+    In,
 
     LayoutStart,
     LayoutSep,
     LayoutEnd,
-
-    #[regex("-?[0-9]+", |lex| lex.slice().parse(), priority = 100)]
-    Number(i32),
-
-    #[regex(r#"[[:^space:]--[\\.()@"]]+"#)]
-    Ident(&'input str),
 
     #[error]
     #[regex(r"[ \n\f]+", logos::skip)]
@@ -38,17 +46,21 @@ impl<'input> fmt::Display for Token<'input> {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         use Token::*;
         match self {
+            Number(n) => n.fmt(fmt),
+            Ident(s) => s.fmt(fmt),
             BackSlash => write!(fmt, "\\"),
             Arrow => write!(fmt, "->"),
             LParen => write!(fmt, "("),
             RParen => write!(fmt, ")"),
+            Equals => write!(fmt, "="),
+            Colon => write!(fmt, ":"),
             Case => write!(fmt, "case"),
             Of => write!(fmt, "of"),
+            Let => write!(fmt, "let"),
+            In => write!(fmt, "in"),
             LayoutStart => write!(fmt, "{{"),
             LayoutSep => write!(fmt, ","),
             LayoutEnd => write!(fmt, "}}"),
-            Number(n) => n.fmt(fmt),
-            Ident(s) => s.fmt(fmt),
             Error => write!(fmt, "error"),
         }
     }
@@ -68,6 +80,8 @@ pub type Spanned<Tok, Loc, Error> = Result<(Loc, Tok, Loc), Error>;
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum Delimiter {
     Paren,
+    LetIn,
+    CaseOf,
 }
 
 #[derive(PartialEq, Eq, Debug)]
@@ -205,9 +219,29 @@ impl<'input> Iterator for Lexer<'input> {
                 }
             }
 
+            Token::Case => {
+                self.collapse(is_offside);
+                self.stack.push(State::Delimiter(Delimiter::CaseOf));
+            }
             Token::Of => {
                 self.collapse(State::is_implicit);
+                if self.stack.last() == Some(&State::Delimiter(Delimiter::CaseOf)) {
+                    self.stack.pop();
+                }
                 self.stack.push(State::LayoutStart(self.line_start));
+            }
+
+            Token::Let => {
+                self.collapse(is_offside);
+                self.stack.push(State::Delimiter(Delimiter::LetIn));
+                self.stack.push(State::LayoutStart(self.line_start));
+            }
+            Token::In => {
+                // Collapse up to and including "let" start marker
+                self.collapse(State::is_implicit);
+                if self.stack.last() == Some(&State::Delimiter(Delimiter::LetIn)) {
+                    self.stack.pop();
+                }
             }
 
             _ => {
@@ -286,6 +320,40 @@ mod tests {
                 (12, Ident("x"), 13),
                 (14, Arrow, 16),
                 (17, Ident("x"), 18),
+            ])
+        );
+    }
+
+    #[test]
+    fn test_lex_nested_let_in() {
+        assert_eq!(
+            Lexer::new(
+                "let
+  x = let y = 0
+          z = y
+      in z in x"
+            )
+            .collect::<Result<_, _>>(),
+            Ok(vec![
+                (0, Let, 3),
+                (6, LayoutStart, 6),
+                (6, Ident("x"), 7),
+                (8, Equals, 9),
+                (10, Let, 13),
+                (14, LayoutStart, 14),
+                (14, Ident("y"), 15),
+                (16, Equals, 17),
+                (18, Number(0), 19),
+                (30, LayoutSep, 30),
+                (30, Ident("z"), 31),
+                (32, Equals, 33),
+                (34, Ident("y"), 35),
+                (42, LayoutEnd, 42),
+                (42, In, 44),
+                (45, Ident("z"), 46),
+                (47, LayoutEnd, 47),
+                (47, In, 49),
+                (50, Ident("x"), 51)
             ])
         );
     }
